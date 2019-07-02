12e1c7a9daff642db4bdc0b90b972fc4337b66fd
root <root@localhost.localdomain>
2019-07-02 18:01:51 +0200

    add chameleon-mini-rebooted from Iceman1001

---
+++ chameleon-mini-rebooted.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for proxmark3
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Iceman1001"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="Based on the open-source NFC tool ChameleonMini: https://github.com/emsec/ChameleonMini but for the ChameleonMini Rebooted"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/iceman1001/ChameleonMini-rebooted"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="ChameleonMiniRebooted"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+ARCH="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+LAUNCHER=""
